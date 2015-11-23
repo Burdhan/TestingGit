a451f30788d367fed03fc8c35e3b47d760d81704
Burhan N <bmnurdin01@gmail.com>
2015-11-22 23:27:11 -0800

    Testing download to pc storage.

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,6 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("Cool");
 		System.out.println("I kinda get it better now");
+		System.out.println("Testing to see if it will download to pc storage.");
 	}
 }
